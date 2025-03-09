0fb9206a7445e89ccef671ff369ad31fbfcaead2
Matheus <matheudbaf0123@gmail.com>
2025-03-09 19:25:10 -0300

    WIP

--- src/pages/home/components/hero/index.tsx
+++ src/pages/home/components/hero/index.tsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 import HeroLogo from "/assets/images/main_logo.png";
+// import stardewbackground from "/assets/images/stardewbackground.png";
 import Parallax from "/assets/images/parallax4.png";
+import Parallax2 from "/assets/images/parallax7.png";
+import Parallax3 from "/assets/images/parallax8.png";
 import Divider from "/assets/images/sdv_divider-s.png";
 import Cloud from "/assets/images/cloud.png";
 import Bird from "/assets/images/stardew_bird.gif";
 import "./styles.sass";
 
 const Hero = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [offsetY, setOffsetY] = useState(0);
 
   const handleScroll = () => {
@@ -20,22 +24,67 @@ const Hero = () => {
   }, []);
 
   return (
-    <div className="hero section">
-      <img className="cloud" src={Cloud} alt="Cloud" />
-      <img className="bird" src={Bird} alt="Bird" />
-      <img
-        className="hero-logo"
-        src={HeroLogo}
-        alt="Stardew Valley"
-        style={{ transform: `translateY(-${offsetY * 0.7}px)` }}
-      />
-      <img
-        className="mountains"
-        src={Parallax}
-        alt="Mountain background"
-        style={{ transform: `translateY(-${offsetY * 0.4}px)` }}
-      />
-      <img className="ground" src={Divider} alt="Divider background" />
+    <div className="hero-container section">
+      <div className="hero">
+        <img
+          className="hero-logo"
+          src={HeroLogo}
+          alt="Stardew Valley"
+          style={{ transform: `translateY(-${offsetY * 0.7}px)` }}
+        />
+        <img
+          className="background"
+          src={'https://stardewvalley.net/wp-content/themes/stardewvalley/images/parallax/parallax1.png'}
+          alt="background"
+          style={{ transform: `translateY(-${offsetY / 8}px)` }}
+          // style={{ transform: `translateY(-${offsetY * 0.12}px)` }}
+          // style={{ bottom:  }}
+        />
+        <img className="cloud" src={Cloud} alt="Cloud" />
+        <img className="bird" src={Bird} alt="Bird" />
+        <div className="stars-container">
+          <img
+            className="stars"
+            src={'https://stardewvalley.net/wp-content/themes/stardewvalley/images/parallax/parallax2.png'}
+            // alt="background"
+            // style={{ transform: `translateY(-${offsetY / 8}px)` }}
+            // style={{ transform: `translateY(-${offsetY * 0.12}px)` }}
+            // style={{ bottom:  }}
+          />
+          <img
+            className="stars"
+            src={'https://stardewvalley.net/wp-content/themes/stardewvalley/images/parallax/parallax2.png'}
+            // alt="background"
+            // style={{ transform: `translateY(-${offsetY / 8}px)` }}
+            // style={{ transform: `translateY(-${offsetY * 0.12}px)` }}
+            // style={{ bottom:  }}
+          />
+        </div>
+        <img
+          className="mountains mountains-back"
+          src={Parallax}
+          alt="Mountain background"
+          style={{ transform: `translateY(${offsetY / 4}px)` }}
+          // style={{ transform: `translateY(-${offsetY * 0.08}px)` }}
+          // style={{ bottom:  }}
+        />
+        <img
+          className="mountains mountains-middle"
+          src={Parallax3}
+          alt="Mountain background"
+          style={{ transform: `translateY(${offsetY / 6.5}px)` }}
+          // style={{ transform: `translateY(-${offsetY * 0.12}px)` }}
+          // style={{ bottom:  }}
+        />
+        <img
+          className="mountains mountains-front"
+          src={Parallax2}
+          alt="Mountain background"
+          style={{ transform: `translateY(${offsetY / 8}px)` }}
+          // style={{ bottom:  }}
+        />
+        <img className="ground" src={Divider} alt="Divider background" />
+      </div>
     </div>
   );
 };
