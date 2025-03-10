978d5813aa06732a964c1f440be4062a7c51f54d
Sophia Portela <sophianobreportela@gmail.com>
2025-03-10 14:38:10 -0300

    improved parallax and minor layout fixes

--- src/pages/home/components/hero/index.tsx
+++ src/pages/home/components/hero/index.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
+import Background from "/assets/images/parallax-background.png";
 import HeroLogo from "/assets/images/main_logo.png";
-import Parallax from "/assets/images/parallax4.png";
+import Parallax1 from "/assets/images/parallax1.png";
+import Parallax2 from "/assets/images/parallax2.png";
+import Parallax3 from "/assets/images/parallax3.png";
 import Divider from "/assets/images/sdv_divider.png";
+import Stars from "/assets/images/stars.png";
 import Cloud from "/assets/images/cloud.png";
 import Bird from "/assets/images/stardew_bird.gif";
 import "./styles.sass";
@@ -20,22 +24,46 @@ const Hero = () => {
   }, []);
 
   return (
-    <div className="hero section">
-      <img className="cloud" src={Cloud} alt="Cloud" />
-      <img className="bird" src={Bird} alt="Bird" />
-      <img
-        className="hero-logo"
-        src={HeroLogo}
-        alt="Stardew Valley"
-        style={{ transform: `translateY(-${offsetY * 0.7}px)` }}
-      />
-      <img
-        className="mountains"
-        src={Parallax}
-        alt="Mountain background"
-        style={{ transform: `translateY(-${offsetY * 0.4}px)` }}
-      />
-      <img className="ground" src={Divider} alt="Divider background" />
+    <div className="hero-container section">
+      <div className="hero">
+        <img
+          className="hero-logo"
+          src={HeroLogo}
+          alt="Stardew Valley"
+          style={{ transform: `translateY(-${offsetY * 0.7}px)` }}
+        />
+        <img
+          className="background"
+          src={Background}
+          alt="background"
+          style={{ transform: `translateY(-${offsetY / 5}px)` }}
+        />
+        <img className="cloud" src={Cloud} alt="Cloud" />
+        <img className="bird" src={Bird} alt="Bird" />
+        <div className="stars-container">
+          <img className="stars" src={Stars} />
+          <img className="stars" src={Stars} />
+        </div>
+        <img
+          className="mountains mountains-back"
+          src={Parallax1}
+          alt="Mountain background"
+          style={{ transform: `translateY(${offsetY / 3.6}px)` }}
+        />
+        <img
+          className="mountains mountains-middle"
+          src={Parallax2}
+          alt="Mountain background"
+          style={{ transform: `translateY(${offsetY / 6.2}px)` }}
+        />
+        <img
+          className="mountains mountains-front"
+          src={Parallax3}
+          alt="Mountain background"
+          style={{ transform: `translateY(${offsetY / 8}px)` }}
+        />
+        <img className="ground" src={Divider} alt="Divider background" />
+      </div>
     </div>
   );
 };
