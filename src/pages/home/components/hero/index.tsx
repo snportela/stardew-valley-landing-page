17c6d2304de42c50ac075fa59b161e3a2d5f2037
Sophia Portela <sophianobreportela@gmail.com>
2025-03-11 22:31:01 -0300

    disabled select on  hero

--- src/pages/home/components/hero/index.tsx
+++ src/pages/home/components/hero/index.tsx
@@ -24,7 +24,7 @@ const Hero = () => {
   }, []);
 
   return (
-    <div className="hero-container section">
+    <div className="hero-container section disable-select">
       <div className="hero">
         <img
           className="hero-logo"
